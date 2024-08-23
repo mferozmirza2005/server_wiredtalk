c49e24681206013ebffca82be9cb8925b5fa35dd
mferozmirza2005 <m.ferozmirza2005@gmail.com>
2024-08-23 22:58:06 +0500

    Transfer websocket code in vercel function.

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import express, { Application, Request, Response } from "express";
-import { Server as SocketIOServer, Socket } from "socket.io";
-import { createServer, Server as HTTPServer } from "http";
 import routes from "./src/routes/routes";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -8,7 +6,6 @@ import cors from "cors";
 dotenv.config();
 
 const app: Application = express();
-const server: HTTPServer = createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,19 +13,8 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false,
   })
 );
-
-const io = new SocketIOServer(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false,
-  },
-});
 
 app.get("/", (req: Request, res: Response): void => {
   res.send("Hello World! from server.");
@@ -36,39 +22,6 @@ app.get("/", (req: Request, res: Response): void => {
 app.use("/api", routes);
 
 const PORT: string | number = process.env.PORT || 5000;
-server.listen(PORT, (): void => {
+app.listen(PORT, (): void => {
   console.log(`Server running at http://localhost:${PORT}/`);
-});
-
-io.on("connection", (socket: Socket) => {
-  socket.on("calling", (data) => {
-    socket.broadcast.emit("calling", data);
-  });
-
-  socket.on("ringing", (data) => {
-    socket.broadcast.emit("ringing", data);
-  });
-
-  socket.on("accepting", (data) => {
-    socket.broadcast.emit("accepting", data);
-  });
-
-  socket.on("declined", (data) => {
-    socket.broadcast.emit("declined", data);
-  });
-
-  socket.on("offer", (offer) => {
-    console.log("Broadcasting offer.");
-    socket.broadcast.emit("offer", offer);
-  });
-
-  socket.on("answer", (answer) => {
-    console.log("Broadcasting answer.");
-    socket.broadcast.emit("answer", answer);
-  });
-
-  socket.on("ice-candidate", (candidate) => {
-    console.log("Broadcasting ICE candidate.");
-    socket.broadcast.emit("ice-candidate", candidate);
-  });
 });
