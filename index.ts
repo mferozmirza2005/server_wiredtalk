2bdf22c122fb88ac1e158c4f961473f3db8163e8
mferozmirza2005 <m.ferozmirza2005@gmail.com>
2024-08-24 17:11:22 +0500

    Updated code for aws deployment and websocket configuration.

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response } from "express";
+import { Server as SocketIOServer, Socket } from "socket.io";
+import { createServer, Server as HTTPServer } from "http";
 import routes from "./src/routes/routes";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -16,12 +18,54 @@ app.use(
   })
 );
 
+const server: HTTPServer = createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
 app.get("/", (req: Request, res: Response): void => {
   res.send("Hello World! from server.");
 });
 app.use("/api", routes);
 
 const PORT: string | number = process.env.PORT || 5000;
-app.listen(PORT, (): void => {
+server.listen(PORT, (): void => {
   console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+io.on("connection", (socket: Socket) => {
+  socket.on("calling", (data) => {
+    socket.broadcast.emit("calling", data);
+  });
+
+  socket.on("ringing", (data) => {
+    socket.broadcast.emit("ringing", data);
+  });
+
+  socket.on("accepting", (data) => {
+    socket.broadcast.emit("accepting", data);
+  });
+
+  socket.on("declined", (data) => {
+    socket.broadcast.emit("declined", data);
+  });
+
+
+  socket.on("offer", (offer) => {
+    console.log("Broadcasting offer.");
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    console.log("Broadcasting answer.");
+    socket.broadcast.emit("answer", answer);
+  });
+
+  socket.on("ice-candidate", (candidate) => {
+    console.log("Broadcasting ICE candidate.");
+    socket.broadcast.emit("ice-candidate", candidate);
+  });
 });
